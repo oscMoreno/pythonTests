508c3ced4e99b2d7d5712cb3e62f0280251cbdff
oscMoreno <osc.moreno1221@gmail.com>
2025-01-30 19:28:08 -0600

    Ejercicios de Recordar

---
+++ Practicas/ejercicios.py
@@ -0,0 +1,19 @@
+# for i in range (11):
+#     print(i)
+
+
+edad = 18 
+if edad >= 18:
+    print('Es mayor de edad')
+else:
+    print('Es menor de edad')
+
+    contador = 0
+    while contador < 5:
+        print(contador)
+        contador +1
+
+a = 2
+b = 2
+
+print('La suma de a + b = ',a+b)
