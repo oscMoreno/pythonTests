1074dbd428e69bb22b61b34e705a7a3c634b6791
oscMoreno <osc.moreno1221@gmail.com>
2025-01-31 19:55:48 -0600

    NotFoundLang

---
+++ Autocorrector/autocorrector.py
@@ -0,0 +1,12 @@
+from autocorrect import Speller
+
+corrector = Speller(lang='es')
+
+def correct_word(word):
+    if not corrector(word) == word:
+        return corrector(word)
+    else:
+        return word
+
+print(correct_word('Me guzta'))
+    
